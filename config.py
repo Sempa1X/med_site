c6e0a83e84e543c9c838373ec69bdf9e0182c6eb
Sempa1X <GodAmong228@yandex.ru>
2021-07-03 15:18:32 +0300

    Sempa1X

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+
+base_dir = os.path.abspath(os.path.dirname(__file__)) 
+
+
+class Config(object):
+    # Default settings
+    FLASK_APP = 'run.py' 
+    DEBUG = True 
+    SECRET_KEY = "Secrets" 
+    #UPLOAD_FOLDER = base_dir + '\\app\\src\\img\\downloads'
+
+    # DataBase settings
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' +\
+        os.path.join(base_dir, 'medic.db') 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
+
