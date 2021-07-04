21d98636f41de515bfb7e69d5f58b256912bab86
Sempa1X <GodAmong228@yandex.ru>
2021-07-04 18:16:18 +0300

    Sempa1X

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from flask import redirect, render_template,\
+    url_for, flash
+
+from app import application as main
+
+
+@main.route('/', methods=['POST', 'GET'])
+def sing_in():
+    return render_template('main/panel.html')
