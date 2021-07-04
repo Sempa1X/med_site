e1ef4b9e49737c1399734028f38438a7288bfd1a
Дмитрий <37273100+shpex@users.noreply.github.com>
2021-07-04 22:33:26 +0300

    1

--- app/database.py
+++ app/database.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(20))
     surname = db.Column(db.String(20))
 
-    sex = db.Column(db.Boolean, default=1)
+    # sex = db.Column(db.Boolean, default=1)
     birthday = db.Column(db.String(50))
     
     phone = db.Column(db.String(50))
@@ -28,38 +28,11 @@ class User(db.Model, UserMixin):
     isActive = db.Column(db.String(50)) # Работаспособность
     isAdmin = db.Column(db.Boolean, default=0)
     isDev =  db.Column(db.Boolean, default=0)
-    
-    coment = db.Column(db.String(255))
-
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-
-    def check_password(self, password):
-        return check_password_hash(self.passwd, password)
-
-
-class Doctor(db.Model, UserMixin):
-    __tablename__ = "doctors"
-    id = db.Column(db.Integer, primary_key=True)
-
-    first_name = db.Column(db.String(20))
-    last_name = db.Column(db.String(20))
-    surname = db.Column(db.String(20))
-
-    birthday = db.Column(db.String(50))
-
     division = db.Column(db.String(255))
     certificate = db.Column(db.String(255))
-    phone3 = db.Column(db.String(255))
+    
+    # coment = db.Column(db.String(255))
 
-    reg_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    isActive = db.Column(db.String(50)) # Работаспособность
-    isAdmin = db.Column(db.Boolean, default=0)
-
-    login = db.Column(db.String(50))
-    passwd = db.Column(db.String(50))
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -69,83 +42,112 @@ class Doctor(db.Model, UserMixin):
         return check_password_hash(self.passwd, password)
 
 
-class Patient(db.Model):
-    __tablename__ = 'patients'
-    id = db.Column(db.Integer, primary_key=True)
+# class Doctor(db.Model, UserMixin):
+#     __tablename__ = "doctors"
+#     id = db.Column(db.Integer, primary_key=True)
 
-    first_name = db.Column(db.String(20))
-    last_name = db.Column(db.String(20))
-    surname = db.Column(db.String(20))
+#     first_name = db.Column(db.String(20))
+#     last_name = db.Column(db.String(20))
+#     surname = db.Column(db.String(20))
 
-    birthday = db.Column(db.String(50))
+#     birthday = db.Column(db.String(50))
 
-    refer = db.Column(db.String(255))
-    pacient_record = db.Column(db.String(255))
+#     division = db.Column(db.String(255))
+#     certificate = db.Column(db.String(255))
+#     phone3 = db.Column(db.String(255))
+
+#     reg_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+#     isActive = db.Column(db.String(50)) # Работаспособность
+#     isAdmin = db.Column(db.Boolean, default=0)
+
+#     login = db.Column(db.String(50))
+#     passwd = db.Column(db.String(50))
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+
+#     def check_password(self, password):
+#         return check_password_hash(self.passwd, password)
+
+
+# class Patient(db.Model):
+#     __tablename__ = 'patients'
+#     id = db.Column(db.Integer, primary_key=True)
+
+#     first_name = db.Column(db.String(20))
+#     last_name = db.Column(db.String(20))
+#     surname = db.Column(db.String(20))
+
+#     birthday = db.Column(db.String(50))
+
+#     refer = db.Column(db.String(255))
+#     pacient_record = db.Column(db.String(255))
     
-    lr_f_name = db.Column(db.String(255))
-    lr_l_name = db.Column(db.String(255))
-    lr_surname = db.Column(db.String(255))
+#     lr_f_name = db.Column(db.String(255))
+#     lr_l_name = db.Column(db.String(255))
+#     lr_surname = db.Column(db.String(255))
 
-    lr_pass_serial = db.Column(db.String(255))
-    lr_pass_num = db.Column(db.String(255))
-    lr_pass_date = db.Column(db.String(255))
-    lr_pass_issued = db.Column(db.String(255))
+#     lr_pass_serial = db.Column(db.String(255))
+#     lr_pass_num = db.Column(db.String(255))
+#     lr_pass_date = db.Column(db.String(255))
+#     lr_pass_issued = db.Column(db.String(255))
     
-    address = db.Column(db.String(255))
-    trust_factor = db.Column(db.Boolean, default=True)
+#     address = db.Column(db.String(255))
+#     trust_factor = db.Column(db.Boolean, default=True)
 
 
-class Child(db.Model):
-    __tablename__ = 'childs'
-    id = db.Column(db.Integer, primary_key=True)
+# class Child(db.Model):
+#     __tablename__ = 'childs'
+#     id = db.Column(db.Integer, primary_key=True)
 
-    first_name = db.Column(db.String(20))
-    last_name = db.Column(db.String(20))
-    surname = db.Column(db.String(20))
+#     first_name = db.Column(db.String(20))
+#     last_name = db.Column(db.String(20))
+#     surname = db.Column(db.String(20))
 
-    birthday = db.Column(db.String(50))
-    lr_status = db.Column(db.String(255))
-    phone4 = db.Column(db.String(255))
+#     birthday = db.Column(db.String(50))
+#     lr_status = db.Column(db.String(255))
+#     phone4 = db.Column(db.String(255))
 
-    refer = db.Column(db.String(255))
-    pacient_record = db.Column(db.String(255))
+#     refer = db.Column(db.String(255))
+#     pacient_record = db.Column(db.String(255))
     
-    lr_f_name = db.Column(db.String(255))
-    lr_l_name = db.Column(db.String(255))
-    lr_surname = db.Column(db.String(255))
+#     lr_f_name = db.Column(db.String(255))
+#     lr_l_name = db.Column(db.String(255))
+#     lr_surname = db.Column(db.String(255))
 
-    lr_pass_serial = db.Column(db.String(255))
-    lr_pass_num = db.Column(db.String(255))
-    lr_pass_date = db.Column(db.String(255))
-    lr_pass_issued = db.Column(db.String(255))
+#     lr_pass_serial = db.Column(db.String(255))
+#     lr_pass_num = db.Column(db.String(255))
+#     lr_pass_date = db.Column(db.String(255))
+#     lr_pass_issued = db.Column(db.String(255))
     
-    address = db.Column(db.String(255))
-    trust_factor = db.Column(db.Boolean, default=True)
+#     address = db.Column(db.String(255))
+#     trust_factor = db.Column(db.Boolean, default=True)
 
 
 
-class Pregnant(db.Model):
-    __tablename__ = 'pregnants'
-    id = db.Column(db.Integer, primary_key=True)
+# class Pregnant(db.Model):
+#     __tablename__ = 'pregnants'
+#     id = db.Column(db.Integer, primary_key=True)
 
-    first_name = db.Column(db.String(20))
-    last_name = db.Column(db.String(20))
-    surname = db.Column(db.String(20))
+#     first_name = db.Column(db.String(20))
+#     last_name = db.Column(db.String(20))
+#     surname = db.Column(db.String(20))
 
-    refer = db.Column(db.String(255))
-    pacient_record = db.Column(db.String(255))
+#     refer = db.Column(db.String(255))
+#     pacient_record = db.Column(db.String(255))
 
-    lr_pass_serial = db.Column(db.String(255))
-    lr_pass_num = db.Column(db.String(255))
-    lr_pass_date = db.Column(db.String(255))
-    lr_pass_issued = db.Column(db.String(255))
+#     lr_pass_serial = db.Column(db.String(255))
+#     lr_pass_num = db.Column(db.String(255))
+#     lr_pass_date = db.Column(db.String(255))
+#     lr_pass_issued = db.Column(db.String(255))
     
-    address = db.Column(db.String(255))
-    trust_factor = db.Column(db.Boolean, default=True)
+#     address = db.Column(db.String(255))
+#     trust_factor = db.Column(db.Boolean, default=True)
 
-    birthday = db.Column(db.String(50))
-    estimated_birthday = db.Column(db.String(255))
-    num_fetus = db.Column(db.Integer)
-    phone5 = db.Column(db.String(255))
+#     birthday = db.Column(db.String(50))
+#     estimated_birthday = db.Column(db.String(255))
+#     num_fetus = db.Column(db.Integer)
+#     phone5 = db.Column(db.String(255))
 
 
--- app/routes.py
+++ app/routes.py
@@ -4,18 +4,42 @@ from flask_login import login_required, current_user,\
     login_user
 from werkzeug.urls import url_parse
 
-from app import application as main
+from app import application
 from app import login
-from .database import User, Doctor, Patient,\
-    Pregnant, Child
+from .database import User
 
 
-@main.route('/', methods=['POST', 'GET'])
+@application.route('/', methods=['POST', 'GET'])
 def sing_in():  
+<<<<<<< HEAD
+    if current_user.is_authenticated:
+        return redirect(url_for('panel'))
+
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('password')
+        user = User.query.filter_by(login=login).first()
+        
+        if user.passwd == password:
+            login_user(user)
+            next_page = request.args.get('next')
+            if not next_page or url_parse(next_page).netloc != '':
+                next_page = url_for('panel')
+            return redirect(next_page)      
+        else:    
+            flash('Invalid username or password')
+            return redirect(url_for('sing_in'))
+
+    return render_template('sing_in.html')
+
+
+@application.route('/panel', methods=['POST', 'GET'])
+=======
     return render_template("main/sign_in.html")
 
 
 @main.route('/panel', methods=['POST', 'GET'])
+>>>>>>> 06fc7776d89d77ba25a4f719106c86992043e0dc
 @login_required
 def panel():
     return 'panel'
