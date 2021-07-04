f84ffa6b926d4d29fbdd27799da9c9d7ba683088
Sempa1X <GodAmong228@yandex.ru>
2021-07-04 20:59:33 +0300

    хкйня

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import redirect, render_template,\
     url_for, flash, request
 from flask_login import login_required, current_user,\
     login_user
+from werkzeug.urls import url_parse
 
 from app import application as main
 from app import login
@@ -11,28 +12,42 @@ from .database import User, Doctor, Patient,\
 
 @main.route('/', methods=['POST', 'GET'])
 def sing_in():  
-    if current_user.is_authenticated:
-        return redirect(url_for('panel'))
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('panel'))
 
     if request.method == 'POST':
-        login = "login"
-        password = "password"
-        user = User.query.filter_by(login=login).first()
+        login = request.form.get('login')
+        password = request.form.get('password')
+        # user = User.query.filter_by(login=login).first()
+        # doc = Doctor.query.filter_by(login=login).first()
+        
+        # if not user.check_password(password):
+        #     #login_user(user)
+        #     return redirect(url_for('panel'))
 
-        if user is None or not user.check_password(password):
-            flash('Invalid username or password')
-            return redirect(url_for('sing_in'))
-        login_user(user)
-        return redirect(url_for('panel'))
-            
-    return render_template('main/panel.html')
+        # elif not doc.check_password(password):
+        #     #login_user(doc)
+        #     return redirect(url_for('panel'))
+        
+        # # else:    
+        # #     flash('Invalid username or password')
+        # #     return redirect(url_for('sing_in'))
+        
+
+        # next_page = request.args.get('next')
+        # if not next_page or url_parse(next_page).netloc != '':
+        #     next_page = url_for('index')
+        # return redirect(next_page)      
+    return render_template('sing_in.html')
 
 
 @main.route('/panel', methods=['POST', 'GET'])
+#@login_required
 def panel():
     return 'panel'
 
 
 @login.user_loader
+@login_required
 def load_user(id):
     return User.query.get(int(id))
