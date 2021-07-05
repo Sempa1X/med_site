6c693cb4a57cfe11e4b6a1c0c4362004d5431f1b
Дмитрий <37273100+shpex@users.noreply.github.com>
2021-07-05 21:06:43 +0300

    update

--- app/routes.py
+++ app/routes.py
@@ -115,7 +115,28 @@ def logout():
 
 
 
+@application.errorhandler(404)
+def error_404(error):
+    return render_template("main/404.html", error=error)
 
+
+@login_required
+@application.route("/panel", methods=['GET', 'POST'])
+def panel():
+    return render_template("main/panel.html", user_role='doctor')
+
+
+@login_required
+@application.route("/record", methods=['GET', 'POST'])
+def record():
+    return render_template("main/doctor_record.html", user_role='doctor')
+
+
+@login_required
+@application.route("/reception", methods=['GET', 'POST'])
+def reception():
+    aa = [["Трунов Д.И.", "18:00", 0, "Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор слов, но это не совсем так. Его корни уходят в один фрагмент классической латыни 45 года н.э., то есть более двух тысячелетий назад. Ричард МакКлинток, профессор латыни из колледжа Hampden-Sydney, штат Вирджиния, взял одно из самых странных слов в Lorem Ipsum, 'consectetur', и занялся его поисками в классической латинской литературе. В результате он нашёл неоспоримый первоисточник Lorem Ipsum в разделах 1.10.32 и 1.10.33 книги 'de Finibus Bonorum et Malorum' ('О пределах добра и зла'), написанной Цицероном в 45 году н.э. Этот трактат по теории этики был очень популярен в эпоху Возрождения. Первая строка Lorem Ipsum,, происходит от одной из строк в разделе 1.10.32"], ["Азанов И.Е.", "19:00", 1, "Пизда заместо члена"]]
+    return render_template("main/doctor_reception.html", user_role='doctor', receptions=aa)
 
 
 
