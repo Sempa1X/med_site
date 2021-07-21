963d4e72b08abaac8a7f43b001f9208739c34ece
Дмитрий <37273100+shpex@users.noreply.github.com>
2021-07-21 21:09:19 +0300

    update

--- app/blocks/add_patient.py
+++ app/blocks/add_patient.py
@@ -23,7 +23,7 @@ def added():
     for i in request.form:
         data_replace.append([i, request.form[i]])
     
-    full_name = data_replace[1][1] + " " + data_replace[2][1] + " " + data_replace[3][1]
+    full_name = f'{data_replace[1][1]} {data_replace[2][1]} {data_replace[3][1]}'
     patient = Patient(full_name=full_name, patient_role=data_replace[0][1], surname=data_replace[1][1], first_name=data_replace[2][1], last_name=data_replace[3][1], birthday=data_replace[4][1],\
         refer=data_replace[5][1], address=data_replace[6][1], phone=data_replace[7][1], phone2=data_replace[8][1], lr_surname=data_replace[9][1],\
         lr_f_name=data_replace[10][1], lr_l_name=data_replace[11][1], lr_status=data_replace[12][1], lr_pass_num=data_replace[13][1], lr_pass_serial=data_replace[14][1],\
