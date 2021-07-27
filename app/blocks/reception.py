16022600c14fe452f012a868d41f0cc1eb6fe7c2
Sempa1X <68701459+Sempa1X@users.noreply.github.com>
2021-07-28 01:26:49 +0300

    upd

--- app/blocks/reception.py
+++ app/blocks/reception.py
@@ -37,28 +37,36 @@ def add_schedule():
     
 @bp_reception.route('/get_doctors', methods=["POST"])
 def get_doctors():
-    current_date = request.form['date']
-    current_date_obj = now.strptime(current_date, '%Y-%m-%d')
-    data_list = []
-    rec_data = []
-    doctors = User.query.filter(and_(User.role == 'doctor'))
-    for i in doctors:
-        for o in i.records: 
-            date_obj = now.strptime(o.date, '%Y-%m-%d')
-            if date_obj == current_date_obj and i.id == o.doctor_id:
-                rec_data.append([{'rec_id': o.id, 'office': o.office, 'date': o.date, 'time': o.time, 'patient_full_name':o.patient_full_name, 'patient_id': o.patient_id}])  
-        data_list.append({'doc_id': i.id, 'doc_full_name': i.full_name, 'records': rec_data}) 
-    print(data_list)
-    return jsonify({'success': 'true', 'data': data_list}) if len(data_list) > 0 else jsonify({'success': 'false'})
-   
+    current_date = str(now.strftime('%Y-%m-%d'))
+    if request.form['date']:
+        current_date = request.form['date']
+        current_date_obj = now.strptime(current_date, '%Y-%m-%d')
+        data_list = []
+        rec_data = []
+        doctors = User.query.filter(and_(User.role == 'doctor'))
+        for i in doctors:
+            for o in i.records: 
+                date_obj = now.strptime(o.date, '%Y-%m-%d')
+                if date_obj == current_date_obj and i.id == o.doctor_id:
+                    rec_data.append({'rec_id': o.id, 'office': o.office, 'date': o.date, 'time': o.time, 'patient_full_name':o.patient_full_name, 'patient_id': o.patient_id})  
+            data_list.append({'doc_id': i.id, 'doc_full_name': i.full_name, 'records': rec_data}) 
+        return jsonify({'success': 'true', 'data': data_list}) if len(data_list) > 0 else jsonify({'success': 'false'})
+    return jsonify({'success': 'false'})
+
+
+
+
 # Work
 @bp_reception.route('/reception_process', methods=["POST"])
 def reception_process():
-    record = Record.query.filter(Record.date == request.form['date'])
     patient_info = []
-    for i in record:
-        patient = Patient.query.get(i.patient_id)
-        patient_info.append({'full_name': patient.full_name, 'trust_factor': patient.trust_factor, 'role': patient.patient_role, 'comment': patient.comment, 'phone': patient.phone}) 
+    if request.form['date']:
+        record = Record.query.filter(Record.date == request.form['date'])
+        for i in record:
+            if i.patient_id:
+                patient = Patient.query.get(i.patient_id)
+                print(patient)
+                patient_info.append({'full_name': patient.full_name, 'trust_factor': patient.trust_factor, 'role': patient.patient_role, 'comment': patient.comment, 'phone': patient.phone}) 
     if len(patient_info) == 0:
         return jsonify({'success': 'false'})
     return jsonify({'success': 'true', 'data': patient_info})
