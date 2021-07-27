a779c88be47c090b35b1dab269ad6684af60a420
Sempa1X <68701459+Sempa1X@users.noreply.github.com>
2021-07-28 01:51:01 +0300

    upd

--- app/blocks/reception.py
+++ app/blocks/reception.py
@@ -25,7 +25,7 @@ def reception():
 # Work
 @bp_reception.route('/add_schedule', methods=['get', "POST"])
 def add_schedule():
-    data = [[1, '2021-07-26', '16:30', 2], [1, '2021-07-26', '16:00', 2], [1, '2021-07-26', '13:30', 2], [1, '2021-07-26', '15:30', 2]]     # request.form['data']
+    data = request.form['data']
     is_added = False 
     for i in data:
         record = Record(doctor_id=i[0], date=i[1], time=i[2], office=i[3])
@@ -42,18 +42,16 @@ def get_doctors():
         current_date = request.form['date']
         current_date_obj = now.strptime(current_date, '%Y-%m-%d')
         data_list = []
+        rec_data = []
         doctors = User.query.filter(and_(User.role == 'doctor'))
         for i in doctors:
-            rec_data = []
             for o in i.records: 
                 date_obj = now.strptime(o.date, '%Y-%m-%d')
                 if date_obj == current_date_obj and i.id == o.doctor_id:
                     rec_data.append({'rec_id': o.id, 'office': o.office, 'date': o.date, 'time': o.time, 'patient_full_name':o.patient_full_name, 'patient_id': o.patient_id})  
-            data_list.append({'doc_id': i.id, 'doc_full_name': i.full_name, 'records': rec_data})
+            data_list.append({'doc_id': i.id, 'doc_full_name': i.full_name, 'records': rec_data}) 
         return jsonify({'success': 'true', 'data': data_list}) if len(data_list) > 0 else jsonify({'success': 'false'})
     return jsonify({'success': 'false'})
-
-
 
 
 # Work
