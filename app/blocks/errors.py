f7ce4c5b3fefc23f01aff87fb69744c5c299f6f5
Sempa1X <68701459+Sempa1X@users.noreply.github.com>
2021-07-28 12:25:10 +0300

    upd

--- app/blocks/errors.py
+++ app/blocks/errors.py
@@ -1,11 +1,16 @@
 from flask import Blueprint, render_template, redirect,\
     url_for, request, flash, jsonify
 from sqlalchemy import or_
-from flask_login import current_user
+from flask_login import current_user, login_required
+
+from app import application
+
 
 bp_error = Blueprint('errors', __name__, url_prefix='/')
 
 
-@bp_error.errorhandler(404)
+@application.errorhandler(404)
+@login_required
 def error_404(error):
     return render_template("error/404.html", error=error), 404
+
--- app/blocks/list_expectation.py
+++ app/blocks/list_expectation.py
@@ -6,7 +6,7 @@ from flask_login import login_required
 from sqlalchemy import or_, and_
 
 from app import db
-from app.src.database import User, Patient, Record
+from app.src.database import User, Patient, Record, List_expectation
 from app.src import age
 
 
@@ -22,6 +22,17 @@ def list_expectation():
     return render_template('list_expectation/list_expectation.html')
 
 
+@bp_list_expectation.route('/record_list', methods=['POST'])
+@login_required 
+def list_get():
+    status = True
+    all_list_dict = []
+    all_list = List_expectation.query.all()
+    for i in all_list:
+        all_list_dict.append(i)
+    return jsonify({'success': 'true', 'all_list': all_list_dict}) if status else  jsonify({'success': 'false'})
+
+
 @bp_list_expectation.route('/add_list', methods=['POST'])
 @login_required 
 def list_add(): 
@@ -31,11 +42,5 @@ def list_add():
 @bp_list_expectation.route('/del_list', methods=['POST'])
 @login_required
 def list_del(): 
-    return jsonify({'success': 'true'})
-
-
-@bp_list_expectation.route('/record_list', methods=['POST'])
-@login_required
-def list_record(): 
     return jsonify({'success': 'true'})
 
