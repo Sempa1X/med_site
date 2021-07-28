8231cf4bafcd79da2c753fb515e20a3fb5ff39ca
Sempa1X <68701459+Sempa1X@users.noreply.github.com>
2021-07-28 07:26:42 +0300

    upd

--- app/blocks/reception.py
+++ app/blocks/reception.py
@@ -40,19 +40,25 @@ def add_schedule():
 def get_doctors():
     current_date = str(now.strftime('%Y-%m-%d'))
     if 'date' in  request.form:
+        patient_data = []
         current_date = request.form['date']
         current_date_obj = now.strptime(current_date, '%Y-%m-%d')
         data_list = []
         doctors = User.query.filter(and_(User.role == 'doctor'))
+        all_patients = Patient.query.all()
+        for i in all_patients:
+            patient_data.append({ 'patient_full_name':o.patient_full_name, 'patient_id': o.patient_id})
         for i in doctors:
             rec_data = []
-            patient_data = []
             for o in i.records: 
                 date_obj = now.strptime(o.date, '%Y-%m-%d')
                 if date_obj == current_date_obj and i.id == o.doctor_id:
-                    patient_data.append({ 'patient_full_name':o.patient_full_name, 'patient_id': o.patient_id})
                     rec_data.append({'rec_id': o.id, 'is_active': o.isActive, 'office': o.office, 'date': o.date, 'time': o.time})  
             data_list.append({'doc_id': i.id, 'doc_full_name': i.full_name, 'records': rec_data, 'patients': patient_data})
         return jsonify({'success': 'true', 'data': data_list}) if len(data_list) > 0 else jsonify({'success': 'false'})
     return jsonify({'success': 'false'})
 
+
+@bp_reception.route('/is_active', methods=["POST"])
+def is_active():
+    if ['rec_id', 'type'] in request.form:
--- app/src/requests.py
+++ app/src/requests.py
@@ -1,7 +1,14 @@
+from flask import render_template
+
 from app import application, db, login
+
 
 @application.before_request
 def before_request():
     pass
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('login'))    
+
+
+@application.errorhandler(404)
+def error_404(error):
+    return render_template("error/404.html", error=error)
+
