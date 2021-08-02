04a801f8fafce734b9c162593b691c3e54501b5a
Дмитрий <37273100+shpex@users.noreply.github.com>
2021-08-02 22:06:49 +0300

    ыыы

--- app/blocks/search.py
+++ app/blocks/search.py
@@ -34,7 +34,7 @@ def search_process():
         patients_search = Patient.query.filter(or_(Patient.full_name.contains(search), Patient.phone.contains(search), Patient.phone2.contains(search)))
         for i in patients_search:
             for o in i.records:
-                obj_date = now.strptime(o.date, '%Y-%m-%d')
+                obj_date = now.strptime(o.date, '%d.%m.%Y')
                 if obj_date >= obj_current_date:
                     future_rec.append({'id': o.id, 'is_active': o.isActive, 'doctor_full_name':o.doctor_full_name, 'date':o.date, 'time':o.time, 'office':o.office,  'doctor_id':o.doctor_id})
                 else:
@@ -42,6 +42,3 @@ def search_process():
             patient_info.append({'address': i.address, 'estimated_birthday': i.estimated_birthday, 'num_fetus': i.num_fetus, 'lr_pass_issued': i.lr_pass_issued, 'lr_pass_date': i.lr_pass_date, 'lr_pass_num': i.lr_pass_num, 'lr_pass_serial': i.lr_pass_serial, 'lr_status': i.lr_status, 'lr_full_name':"{} {} {}".format(i.lr_surname, i.lr_f_name, i.lr_l_name),'is_reception': i.is_reception,'birthday': i.birthday, 'full_name': i.full_name, 'comment': i.comment,'trust_factor': i.trust_factor, 'role': i.patient_role, 'phone': i.phone, 'phone2': i.phone2, 'lost_records': lost_rec, 'future_records': future_rec, 'age': age.calculate_age(int(i.birthday.split(".")[2]), int(i.birthday.split(".")[1]), int(i.birthday.split(".")[0]))}) 
         return jsonify({'success': 'false', 'text': 'Нет пациента'}) if len(patient_info) == 0 else jsonify({'success': 'true', 'patients': patient_info})
     return jsonify({'success': 'false', 'text': 'Нет пациента'})
-    
-
-
